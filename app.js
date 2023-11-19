7a1bca57c308309f4eb181ca3ac22eec260724db
khoihaylam <khoimapdit1234@gmail.com>
2023-11-19 14:07:59 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+let s2=0
+for(let i=0;i<1000;i++){
+    if(i%2==1){
+        s2=s2+i
+    }
+}
+console.log(s2)
+let count=0
+let s4=0
+// let count1=1
+while (count<1000){
+    if(count%2==1){
+        s4=s4+count
+    }
+    count++
+}
+console.log(s4)
